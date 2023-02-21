5b788137224404196e4eee416762b09fdedc72e2
Young-prof <frankiperezi99@gmail.com>
2023-02-21 11:52:42 -0800

    First commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - nothing, return void
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	putchar('\n');
+	return (0);
+}
